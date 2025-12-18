1c05d2ece4cf343ee38514540a7c09e079f237db
Aschylo <120350803+Aschylo@users.noreply.github.com>
2025-12-18 22:08:34 +0700

    Update cards.js

--- cards.js
+++ cards.js
@@ -268,7 +268,9 @@ const decks = {
   { name: "What a Terrible Big Mouth You Have", count: 2, image: "https://yptpnirqgfmxphjvsdjz.supabase.co/storage/v1/object/public/decks/MFk-_yPhM6jcizrUuYdYu.webp" },
   { name: "Never Leave the Path", count: 2, image: "https://yptpnirqgfmxphjvsdjz.supabase.co/storage/v1/object/public/decks/rBFJyAYYcJv8l_mG4E3p0.webp" },
   { name: "A Grimm Tale", count: 2, image: "https://yptpnirqgfmxphjvsdjz.supabase.co/storage/v1/object/public/decks/Z_vhyAWFFIwqE5Y9JHiHi.webp" },
-  { name: "Into the Woods", count: 3, image: "https://yptpnirqgfmxphjvsdjz.supabase.co/storage/v1/object/public/decks/zbor2pw7IucurrmUXBHLP.webp" }
+  { name: "Into the Woods", count: 3, image: "https://yptpnirqgfmxphjvsdjz.supabase.co/storage/v1/object/public/decks/zbor2pw7IucurrmUXBHLP.webp" },
+  { name: "Into the Woods (rose)", count: 1, image: "little-red/Rose.png" },
+  { name: "Into the Woods (sword)", count: 1, image: "little-red/Sword.png" }
   ],
 
   "Buffy": [
@@ -1062,4 +1064,5 @@ const TitaniaGlamourDeck = [
   { name : "Glamour of Rhyme", image: "glamour/Rhyme.png"},
   { name : "Glamour of Sleep", image: "glamour/Sleep.png"}
 ];
+
   
